dcab529e56309f4c992a59e6d1c0a672e6d0c694
Ruturajn <nanotiruturaj@gmail.com>
2024-05-23 18:59:51 -0400

    update: move parser function definitions from the header to the source file.
    Added an overloaded `expect` function that accepts a const token reference, for allowing the
    user to pass in a specific token.

--- inc/parser.h
+++ inc/parser.h
@@ -18,6 +18,13 @@ public:
     ssize_t curr_pos;
     ssize_t tok_len;
     std::unordered_map<Token::TokType, int> precedence;
+    static const std::string global_key;
+    Program prog;
+    enum DeclType {
+        DECL_TYPE_INT,
+        DECL_TYPE_STRING,
+        DECL_TYPE_BOOL,
+    };
 
     // This map binds function names to their local scope, which itself is a map
     // that binds variables used in the local scope to their type.
@@ -26,326 +33,24 @@ public:
     std::unordered_map<std::string,
         std::unordered_map<std::string, Type*>> env;
 
-    static const std::string global_key;
-
-    Program prog;
-
-    enum DeclType {
-        DECL_TYPE_INT,
-        DECL_TYPE_STRING,
-        DECL_TYPE_BOOL,
-    };
-
-    enum DeclType conv_type(enum Token::TokType t_ty) {
-        switch (t_ty) {
-            case Token::TOK_TYPE_INT:
-                return DECL_TYPE_INT;
-
-            case Token::TOK_TYPE_STRING:
-                return DECL_TYPE_STRING;
-
-            case Token::TOK_TYPE_BOOL:
-                return DECL_TYPE_BOOL;
-
-            default:
-                std::cout << "[ERR]: Invalid syntax at decl type at: " <<
-                    "[" << tok_list[curr_pos].line_num << "," <<
-                    tok_list[curr_pos].col_num << "]\n";
-                return DECL_TYPE_INT;
-        }
-    }
-
-    Parser(std::vector<Token>& _tok_list, const std::string& _file_data,
-            const std::string& _file_name)
-        : file_name(_file_name), file_lines({}), tok_list(_tok_list), curr_pos(0),
-            tok_len(_tok_list.size()), precedence({}), prog(Program()) {
-
-        precedence[Token::TOK_MULT] = 100;
-        precedence[Token::TOK_DIV] = 100;
-        precedence[Token::TOK_MODULUS] = 100;
-
-        precedence[Token::TOK_PLUS] = 80;
-        precedence[Token::TOK_MINUS] = 80;
-
-        precedence[Token::TOK_LSHIFT] = 60;
-        precedence[Token::TOK_RSHIFT] = 60;
-
-        precedence[Token::TOK_LT] = 40;
-        precedence[Token::TOK_LTE] = 40;
-        precedence[Token::TOK_GT] = 40;
-        precedence[Token::TOK_GTE] = 40;
-
-        precedence[Token::TOK_EQEQUAL] = 20;
-        precedence[Token::TOK_NEQUAL] = 20;
-
-        precedence[Token::TOK_BITAND] = 10;
-
-        precedence[Token::TOK_BITXOR] = 7;
-
-        precedence[Token::TOK_BITOR] = 5;
-
-        precedence[Token::TOK_LOGAND] = 5;
-
-        precedence[Token::TOK_LOGOR] = 3;
-
-        std::istringstream stream(_file_data);
-        std::string line;
-
-        // Traverse each line using std::getline
-        while (std::getline(stream, line)) {
-            file_lines.push_back(line);
-        }
-    }
-
-    Token::TokType check_next() const {
-        if (curr_pos >= tok_len)
-            return Token::TOK_EOF;
-
-        if ((curr_pos + 1) >= tok_len)
-            return Token::TOK_EOF;
-
-        return tok_list[curr_pos + 1].tok_ty;
-    }
-
-    bool is_next_binop() const {
-        Token::TokType next_tok = check_next();
-        switch (next_tok) {
-            case Token::TOK_PLUS:
-            case Token::TOK_MINUS:
-            case Token::TOK_MULT:
-            case Token::TOK_DIV:
-            case Token::TOK_LSHIFT:
-            case Token::TOK_RSHIFT:
-            case Token::TOK_MODULUS:
-            case Token::TOK_BITAND:
-            case Token::TOK_BITOR:
-            case Token::TOK_BITXOR:
-            case Token::TOK_EQEQUAL:
-            case Token::TOK_NEQUAL:
-            case Token::TOK_GT:
-            case Token::TOK_LT:
-            case Token::TOK_GTE:
-            case Token::TOK_LTE:
-            case Token::TOK_LOGAND:
-            case Token::TOK_LOGOR:
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    int get_precedence(enum Token::TokType t_ty) {
-        if (precedence.find(t_ty) != precedence.end())
-            return precedence[t_ty];
-
-        return -1;
-    }
-
-    enum BinopExpNode::BinopType conv_binop(const Token& t) const {
-        switch (t.tok_ty) {
-            case Token::TOK_PLUS:
-                return BinopExpNode::BINOP_PLUS;
-
-            case Token::TOK_MINUS:
-                return BinopExpNode::BINOP_MINUS;
-
-            case Token::TOK_MULT:
-                return BinopExpNode::BINOP_MULT;
-
-            case Token::TOK_DIV:
-                return BinopExpNode::BINOP_DIVIDE;
-
-            case Token::TOK_LSHIFT:
-                return BinopExpNode::BINOP_LSHIFT;
-
-            case Token::TOK_RSHIFT:
-                return BinopExpNode::BINOP_RSHIFT;
-
-            case Token::TOK_MODULUS:
-                return BinopExpNode::BINOP_MODULUS;
-
-            case Token::TOK_BITAND:
-                return BinopExpNode::BINOP_BITAND;
-
-            case Token::TOK_BITOR:
-                return BinopExpNode::BINOP_BITOR;
-
-            case Token::TOK_BITXOR:
-                return BinopExpNode::BINOP_BITXOR;
-
-            case Token::TOK_EQEQUAL:
-                return BinopExpNode::BINOP_EQEQUAL;
-
-            case Token::TOK_NEQUAL:
-                return BinopExpNode::BINOP_NEQUAL;
-
-            case Token::TOK_GT:
-                return BinopExpNode::BINOP_GT;
-
-            case Token::TOK_LT:
-                return BinopExpNode::BINOP_LT;
-
-            case Token::TOK_GTE:
-                return BinopExpNode::BINOP_GTE;
-
-            case Token::TOK_LTE:
-                return BinopExpNode::BINOP_LTE;
-
-            case Token::TOK_LOGAND:
-                return BinopExpNode::BINOP_LOGAND;
-
-            case Token::TOK_LOGOR:
-                return BinopExpNode::BINOP_LOGOR;
-
-            default:
-                std::cout << "[ERR]: Encountered invalid token for binop "
-                    "conversion at: " <<
-                    "[" << t.line_num << "," << t.col_num << "]\n";
-                return BinopExpNode::BINOP_PLUS;
-        }
-    }
-
-    enum UnopExpNode::UnopType conv_unop(const Token& t) const {
-        switch (t.tok_ty) {
-            case Token::TOK_NEG:
-                return UnopExpNode::UNOP_NEG;
-
-            case Token::TOK_NOT:
-                return UnopExpNode::UNOP_NOT;
-
-            case Token::TOK_DEREF:
-                return UnopExpNode::UNOP_DEREF;
-
-            case Token::TOK_ADDROF:
-                return UnopExpNode::UNOP_ADDROF;
-
-            default:
-                std::cout << "[ERR]: Encountered invalid token for binop "
-                    "conversion at: " <<
-                    "[" << t.line_num << "," << t.col_num << "]\n";
-                return UnopExpNode::UNOP_NEG;
-        }
-    }
-
     void advance() { curr_pos += 1; }
-
-    bool expect(Token::TokType t_ty, const char* error_str) {
-        if (curr_pos >= tok_len)
-            return false;
-
-        if (tok_list[curr_pos].tok_ty != t_ty) {
-            ssize_t l_n = tok_list[curr_pos].line_num;
-            ssize_t c_n = tok_list[curr_pos].col_num;
-            std::string tok_str = tok_list[curr_pos].lexeme;
-
-            if (curr_pos > 0) {
-                l_n = tok_list[curr_pos - 1].line_num;
-                c_n = tok_list[curr_pos - 1].col_num;
-                tok_str = tok_list[curr_pos - 1].lexeme;
-            }
-
-            std::cout << "\033[1;31m[ERR]:\033[1;37m " << error_str << "\n";
-            std::cout << ">> " << file_name << "[" << l_n << "," << c_n << "]\n";
-
-            std::cout << std::setw(8) << tok_list[curr_pos].line_num << " | ";
-
-            for (int i = 0; i < c_n - 1; i++)
-                std::cout << file_lines[l_n - 1][i];
-
-            std::cout << "\033[1;31m" << tok_str << "\033[1;37m";
-
-            for (int i = (c_n + tok_str.size()) - 1; i < (int)(file_lines[l_n - 1].size()); i++)
-                std::cout << file_lines[l_n - 1][i];
-            std::cout << "\n";
-
-            std::cout << std::setw(8) << " " << " |";
-
-            for (int i = 0; i < c_n; i++)
-                std::cout << ' ';
-
-            std::cout << "\033[1;31m";
-            for (int i = 0; i < (int)tok_str.size(); i++)
-                std::cout << '^';
-
-            std::cout << "\033[1;37m\n\n";
-
-            return false;
-        }
-
-        return true;
-    }
-
+    enum DeclType conv_type(enum Token::TokType t_ty) const;
+    Parser(std::vector<Token>& _tok_list, const std::string& _file_data,
+            const std::string& _file_name);
+    bool expect(Token::TokType t_ty, const char* error_str, const Token& tok) const;
+    bool expect(Token::TokType t_ty, const char* error_str) const;
+    Token::TokType check_next() const;
+    bool is_next_binop() const;
+    int get_precedence(enum Token::TokType t_ty);
+    enum BinopExpNode::BinopType conv_binop(const Token& t) const;
+    enum UnopExpNode::UnopType conv_unop(const Token& t) const;
     void var_bind_ctxt(const std::string& fun_ctxt,
                        const std::string& var_name,
-                       Type* val) {
-
-        if (env.find(fun_ctxt) == env.end())
-            env[fun_ctxt] = {};
-
-        if (env[fun_ctxt].find(var_name) != env[fun_ctxt].end())
-            std::cout << "[ERR]: Redefinition of variable " << var_name <<
-                         " at: " <<
-                         "[" << tok_list[curr_pos].line_num << "," <<
-                         tok_list[curr_pos].col_num << "]\n";
-
-        env[fun_ctxt][var_name] = val;
-    }
-
-    Type* build_type(int in_count, enum DeclType dt) {
-        if (in_count == 0) {
-            switch (dt) {
-                case DECL_TYPE_INT:
-                    return new TInt();
-                    break;
-                case DECL_TYPE_STRING:
-                    return new TString();
-                    break;
-                case DECL_TYPE_BOOL:
-                    return new TBool();
-                    break;
-            }
-        }
-
-        return new TRef(build_type(in_count - 1,dt));
-    }
-
-    Type* parse_type() {
-        enum DeclType type_set = conv_type(tok_list[curr_pos].tok_ty);
-
-        int indirection_count = 0;
-        while (check_next() == Token::TOK_DEREF) {
-            advance();
-            indirection_count += 1;
-        }
-
-        if (check_next() == Token::TOK_LBRACKET) {
-            advance();
-
-            advance();
-            expect(Token::TOK_RBRACKET, "`]` for ending array decl");
-
-            return new TArray(build_type(indirection_count, type_set));
-        }
-
-        auto p_type = build_type(indirection_count, type_set);
-        return p_type;
-    }
-
-    Type* parse_type_wo_arr() {
-        enum DeclType type_set = conv_type(tok_list[curr_pos].tok_ty);
-
-        int indirection_count = 0;
-        while (check_next() == Token::TOK_DEREF) {
-            advance();
-            indirection_count += 1;
-        }
-
-        auto p_type = build_type(indirection_count, type_set);
-        return p_type;
-    }
-
+                       Type* val);
+    Type* build_type(int in_count, enum DeclType dt);
+    Type* parse_type();
+    Type* parse_type_wo_arr();
     std::unique_ptr<ExpNode> parse_new_type_exp();
-
     std::unique_ptr<ExpNode> parse_binop(int prev_prec,
                                          std::unique_ptr<ExpNode> lhs);
     std::unique_ptr<ExpNode> parse_expr(int prev_prec);
--- src/parser.cpp
+++ src/parser.cpp
@@ -3,6 +3,311 @@
 
 const std::string Parser::global_key = "__global__";
 
+///===-------------------------------------------------------------------===///
+/// Helper Functions
+///===-------------------------------------------------------------------===///
+
+enum Parser::DeclType Parser::conv_type(enum Token::TokType t_ty) const {
+    switch (t_ty) {
+        case Token::TOK_TYPE_INT:
+            return DECL_TYPE_INT;
+
+        case Token::TOK_TYPE_STRING:
+            return DECL_TYPE_STRING;
+
+        case Token::TOK_TYPE_BOOL:
+            return DECL_TYPE_BOOL;
+
+        default:
+            expect(Token::TOK_EOF, "Invalid syntax for decl type");
+            return DECL_TYPE_INT;
+    }
+}
+
+Parser::Parser(std::vector<Token>& _tok_list, const std::string& _file_data,
+        const std::string& _file_name)
+    : file_name(_file_name), file_lines({}), tok_list(_tok_list), curr_pos(0),
+        tok_len(_tok_list.size()), precedence({}), prog(Program()) {
+
+    precedence[Token::TOK_MULT] = 100;
+    precedence[Token::TOK_DIV] = 100;
+    precedence[Token::TOK_MODULUS] = 100;
+
+    precedence[Token::TOK_PLUS] = 80;
+    precedence[Token::TOK_MINUS] = 80;
+
+    precedence[Token::TOK_LSHIFT] = 60;
+    precedence[Token::TOK_RSHIFT] = 60;
+
+    precedence[Token::TOK_LT] = 40;
+    precedence[Token::TOK_LTE] = 40;
+    precedence[Token::TOK_GT] = 40;
+    precedence[Token::TOK_GTE] = 40;
+
+    precedence[Token::TOK_EQEQUAL] = 20;
+    precedence[Token::TOK_NEQUAL] = 20;
+
+    precedence[Token::TOK_BITAND] = 10;
+
+    precedence[Token::TOK_BITXOR] = 7;
+
+    precedence[Token::TOK_BITOR] = 5;
+
+    precedence[Token::TOK_LOGAND] = 5;
+
+    precedence[Token::TOK_LOGOR] = 3;
+
+    std::istringstream stream(_file_data);
+    std::string line;
+
+    // Traverse each line using std::getline
+    while (std::getline(stream, line)) {
+        file_lines.push_back(line);
+    }
+}
+
+Token::TokType Parser::check_next() const {
+    if (curr_pos >= tok_len)
+        return Token::TOK_EOF;
+
+    if ((curr_pos + 1) >= tok_len)
+        return Token::TOK_EOF;
+
+    return tok_list[curr_pos + 1].tok_ty;
+}
+
+bool Parser::is_next_binop() const {
+    Token::TokType next_tok = check_next();
+    switch (next_tok) {
+        case Token::TOK_PLUS:
+        case Token::TOK_MINUS:
+        case Token::TOK_MULT:
+        case Token::TOK_DIV:
+        case Token::TOK_LSHIFT:
+        case Token::TOK_RSHIFT:
+        case Token::TOK_MODULUS:
+        case Token::TOK_BITAND:
+        case Token::TOK_BITOR:
+        case Token::TOK_BITXOR:
+        case Token::TOK_EQEQUAL:
+        case Token::TOK_NEQUAL:
+        case Token::TOK_GT:
+        case Token::TOK_LT:
+        case Token::TOK_GTE:
+        case Token::TOK_LTE:
+        case Token::TOK_LOGAND:
+        case Token::TOK_LOGOR:
+            return true;
+        default:
+            return false;
+    }
+}
+
+int Parser::get_precedence(enum Token::TokType t_ty) {
+    if (precedence.find(t_ty) != precedence.end())
+        return precedence[t_ty];
+
+    return -1;
+}
+
+enum BinopExpNode::BinopType Parser::conv_binop(const Token& t) const {
+    switch (t.tok_ty) {
+        case Token::TOK_PLUS:
+            return BinopExpNode::BINOP_PLUS;
+
+        case Token::TOK_MINUS:
+            return BinopExpNode::BINOP_MINUS;
+
+        case Token::TOK_MULT:
+            return BinopExpNode::BINOP_MULT;
+
+        case Token::TOK_DIV:
+            return BinopExpNode::BINOP_DIVIDE;
+
+        case Token::TOK_LSHIFT:
+            return BinopExpNode::BINOP_LSHIFT;
+
+        case Token::TOK_RSHIFT:
+            return BinopExpNode::BINOP_RSHIFT;
+
+        case Token::TOK_MODULUS:
+            return BinopExpNode::BINOP_MODULUS;
+
+        case Token::TOK_BITAND:
+            return BinopExpNode::BINOP_BITAND;
+
+        case Token::TOK_BITOR:
+            return BinopExpNode::BINOP_BITOR;
+
+        case Token::TOK_BITXOR:
+            return BinopExpNode::BINOP_BITXOR;
+
+        case Token::TOK_EQEQUAL:
+            return BinopExpNode::BINOP_EQEQUAL;
+
+        case Token::TOK_NEQUAL:
+            return BinopExpNode::BINOP_NEQUAL;
+
+        case Token::TOK_GT:
+            return BinopExpNode::BINOP_GT;
+
+        case Token::TOK_LT:
+            return BinopExpNode::BINOP_LT;
+
+        case Token::TOK_GTE:
+            return BinopExpNode::BINOP_GTE;
+
+        case Token::TOK_LTE:
+            return BinopExpNode::BINOP_LTE;
+
+        case Token::TOK_LOGAND:
+            return BinopExpNode::BINOP_LOGAND;
+
+        case Token::TOK_LOGOR:
+            return BinopExpNode::BINOP_LOGOR;
+
+        default:
+            expect(Token::TOK_EOF, "Invalid token for binop conversion", t);
+            return BinopExpNode::BINOP_PLUS;
+    }
+}
+
+enum UnopExpNode::UnopType Parser::conv_unop(const Token& t) const {
+    switch (t.tok_ty) {
+        case Token::TOK_NEG:
+            return UnopExpNode::UNOP_NEG;
+
+        case Token::TOK_NOT:
+            return UnopExpNode::UNOP_NOT;
+
+        case Token::TOK_DEREF:
+            return UnopExpNode::UNOP_DEREF;
+
+        case Token::TOK_ADDROF:
+            return UnopExpNode::UNOP_ADDROF;
+
+        default:
+            expect(Token::TOK_EOF, "Invalid token for unop conversion", t);
+            return UnopExpNode::UNOP_NEG;
+    }
+}
+
+bool Parser::expect(Token::TokType t_ty, const char* error_str) const {
+    return expect(t_ty, error_str, tok_list[curr_pos]);
+}
+
+bool Parser::expect(Token::TokType t_ty, const char* error_str, const Token& tok) const {
+    if (curr_pos >= tok_len)
+        return false;
+
+    if (tok.tok_ty != t_ty) {
+        ssize_t l_n = tok.line_num;
+        ssize_t c_n = tok.col_num;
+        std::string tok_str = tok.lexeme;
+
+        std::cout << "\033[1;31m[ERR]:\033[1;37m " << error_str << "\n";
+        std::cout << ">> " << file_name << "[" << l_n << "," << c_n << "]\n";
+
+        std::cout << std::setw(8) << tok_list[curr_pos].line_num << " | ";
+
+        for (int i = 0; i < c_n - 1; i++)
+            std::cout << file_lines[l_n - 1][i];
+
+        std::cout << "\033[1;31m" << tok_str << "\033[1;37m";
+
+        for (int i = (c_n + tok_str.size()) - 1; i < (int)(file_lines[l_n - 1].size()); i++)
+            std::cout << file_lines[l_n - 1][i];
+        std::cout << "\n";
+
+        std::cout << std::setw(8) << " " << " |";
+
+        for (int i = 0; i < c_n; i++)
+            std::cout << ' ';
+
+        std::cout << "\033[1;31m";
+        for (int i = 0; i < (int)tok_str.size(); i++)
+            std::cout << '^';
+
+        std::cout << "\033[1;37m\n\n";
+
+        return false;
+    }
+
+    return true;
+}
+
+// void Parser::var_bind_ctxt(const std::string& fun_ctxt,
+//                    const std::string& var_name,
+//                    Type* val) {
+// 
+//     if (env.find(fun_ctxt) == env.end())
+//         env[fun_ctxt] = {};
+// 
+//     if (env[fun_ctxt].find(var_name) != env[fun_ctxt].end())
+//         std::cout << "[ERR]: Redefinition of variable " << var_name <<
+//                      " at: " <<
+//                      "[" << tok_list[curr_pos].line_num << "," <<
+//                      tok_list[curr_pos].col_num << "]\n";
+// 
+//     env[fun_ctxt][var_name] = val;
+// }
+
+Type* Parser::build_type(int in_count, enum DeclType dt) {
+    if (in_count == 0) {
+        switch (dt) {
+            case DECL_TYPE_INT:
+                return new TInt();
+                break;
+            case DECL_TYPE_STRING:
+                return new TString();
+                break;
+            case DECL_TYPE_BOOL:
+                return new TBool();
+                break;
+        }
+    }
+
+    return new TRef(build_type(in_count - 1,dt));
+}
+
+Type* Parser::parse_type() {
+    enum DeclType type_set = conv_type(tok_list[curr_pos].tok_ty);
+
+    int indirection_count = 0;
+    while (check_next() == Token::TOK_DEREF) {
+        advance();
+        indirection_count += 1;
+    }
+
+    if (check_next() == Token::TOK_LBRACKET) {
+        advance();
+
+        advance();
+        expect(Token::TOK_RBRACKET, "`]` for ending array decl");
+
+        return new TArray(build_type(indirection_count, type_set));
+    }
+
+    auto p_type = build_type(indirection_count, type_set);
+    return p_type;
+}
+
+Type* Parser::parse_type_wo_arr() {
+    enum DeclType type_set = conv_type(tok_list[curr_pos].tok_ty);
+
+    int indirection_count = 0;
+    while (check_next() == Token::TOK_DEREF) {
+        advance();
+        indirection_count += 1;
+    }
+
+    auto p_type = build_type(indirection_count, type_set);
+    return p_type;
+}
+
+///===-------------------------------------------------------------------===///
+/// Parsing AST Functions
+///===-------------------------------------------------------------------===///
 std::unique_ptr<ExpNode> Parser::parse_binop(int prev_prec,
                                              std::unique_ptr<ExpNode> lhs) {
     Token curr_tok = tok_list[curr_pos];
@@ -399,17 +704,12 @@ StmtNode* Parser::parse_stmt(const std::string& fname) {
             break;
 
         case Token::TOK_IDENT: {
-            /* ssize_t orig = curr_pos; */
 
             auto left = parse_lhs(0);
             advance();
 
-            if (tok_list[curr_pos].tok_ty != Token::TOK_EQUAL) {
-
-                /* curr_pos = orig; */
-
+            if (tok_list[curr_pos].tok_ty != Token::TOK_EQUAL)
                 return parse_sfun_call();
-            }
 
             expect(Token::TOK_EQUAL, "`=` operator for assign statement");
 
